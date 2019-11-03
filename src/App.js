0a35d56fede9df1d53ea8b4d90d4c123e4af0c36
matt investigator <mattinvestigator@gmail.com>
2019-11-02 22:56:23 -0600

    estilos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './ed-grid.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="ed-grid full header">
+        <div class="ed-container full">
+          <header class="ed-container full">
+            <div id="title" class="ed-container">
+              <div class="ed-container">
+                <div class="ed-item s-50 lg-60 contenido">
+                  <p class="s-left m-left lg-left contenido">Portafolio</p>
+                </div>
+                <div class="ed-item s-30 lg-10 contenido info">
+                  <p class="s-left m-center lg-center contenido">1</p>
+                </div>
+                <div class="ed-item s-20 lg-10 contenido fondo">
+                  <p class="s-left m-center lg-center contenido">2</p>
+                </div>
+                <div class="ed-item s-20 lg-10 contenido info">
+                  <p class="s-left m-center lg-center contenido">3</p>
+                </div>
+                <div class="ed-item s-20 lg-10 contenido fondo">
+                  <p class="s-left m-center lg-center contenido">4</p>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+      <div class="ed-grid full stage">
+aaa
+      </div>
+      <div class="ed-grid full stage_2">
+aaa
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+
+*/
